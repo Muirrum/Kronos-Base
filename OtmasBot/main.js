9fcc1b304815de48dad37f71713a4128e73c51b1
Oatmas64134 <Oatmas64134@users.noreply.github.com>
2016-12-13 21:20:52 -0500

    made code more efficent

--- OtmasBot/main.js
+++ OtmasBot/main.js
@@ -31,11 +31,15 @@ client.on('message', message => {
   var prefix = "█"
   if (message.author.bot) return;
   //if (!message.content.startsWith(prefix)) return;
+	
+  let command = message.content.split(" ")[0]
+  command = command.slice(prefix.length)
+  
   if (message.content.startsWith(prefix + 'ping')) {
     console.log('The bot works!')
     message.channel.sendMessage('pong!');
   }
-  if (message.content.startsWith(prefix + 'cmds')) {
+  if (command ==='cmds')) {
     message.channel.sendMessage('Open commands are:')
     message.channel.sendMessage('-ping: Pong!')
     message.channel.sendMessage('-about: Displays some info.')
@@ -43,7 +47,7 @@ client.on('message', message => {
       message.channel.sendMessage('-git: Sends you to the github repository')
       message.channel.sendMessage('-invite: gives you a link to invite the bot to your server')  
   }
-  if(message.content === prefix + 'cat') {	
+  if(command === 'cat') {	
 			var jsonfile = httpGet('http://random.cat/meow')
 			var jsdc = JSON.parse(jsonfile);
 			message.channel.sendMessage(jsdc.file);
@@ -51,10 +55,10 @@ client.on('message', message => {
 			var txt = httpGet('http://random.dog/woof')
 			message.channel.sendMessage('http://random.dog/' + txt);
 	}
-  if (message.content.startsWith(prefix + 'about')) {
+  if (command === 'about')) {
     message.reply('This is OtmasBot Version ' + VERSION + " which was coded by <@118455061222260736> with some help from <@129280647633174528> in just over 5 minutes. For more info, or to report problems, don't message him. :D ")
   }
-  if (message.content.startsWith(prefix + 'authorInfo')) {
+  if (command === 'authorInfo')) {
     message.channel.sendMessage('This is totally not-lying information about @Otmas, the author of this bot.')
     message.channel.sendMessage('Otmas was born in the United States of Otmas, in the city of Otmasington D.C.')
     message.channel.sendMessage('He started programming at the early age of 0 months.')
@@ -62,7 +66,7 @@ client.on('message', message => {
     message.channel.sendMessage('He is also responsible for the destruction of SCP-666 and is personally responsible for the construction of SCPF Site-61')
 	message.channel.sendMessage('Xavier also did some stuff too. I promise. Totes not trying to get fame. 100%. Lots of love. xoxox')
   }
-  if (message.content.startsWith(prefix + 'announce')) {
+  if (command === 'announce')) {
 
     let args = message.content.split(' ')
     let annMsg = args.slice(1).join(" "); // remove first 2 args, then join array with a space
@@ -75,11 +79,11 @@ client.on('message', message => {
     let args = message.content.split(' ')
     prefix = args[1]
   }
-  if (message.content.startsWith(prefix + 'git')) {
+  if (command === 'git')) {
     message.reply('The github repo can be found at https://github.com/Oatmas64134/Otmas-Bot')
     message.reply('Please send all bug reports there, and not to Otmas')
   }
-  if (message.content.startsWith(prefix + 'kick')) {
+  if (command ===  'kick')) {
       let modRole = message.guild.roles.find("name", "OtmasBot Commander")
       if (message.member.roles.has(modRole.id)) {
 	  let args = message.content.split(' ')
@@ -88,11 +92,11 @@ client.on('message', message => {
       } else {
 	  message.reply("Heh. You honestly thought that you could do this? Welp, you can't.")
   }
-    if (message.content.startsWith(prefix + 'uptime')) {
+    if (command === 'uptime')) {
 	message.reply(client.uptime)
 	console.log('Almighty Otmas, the current uptime is:' + client.uptime)
     }
-    if (message.content.startsWith(prefix + 'nickname')) {
+    if (command === 'nickname')) {
 	let args = message.content.split(' ' )
 	let botNick = args.slice(1).join(" ")
         var bot = Discord.GuildMember
@@ -100,7 +104,7 @@ client.on('message', message => {
 	console.log(botNick)
     }
   }
-    if (message.content.startsWith(prefix + 'eval')) {
+    if (command === 'eval')) {
         if(message.author.id !== "118455061222260736") return;
 	try {
 	    let args = message.content.split(' ')
@@ -116,7 +120,7 @@ client.on('message', message => {
 	}
 
     }
-    if (message.content.startsWith(prefix + 'invite')) {
+    if (command === 'invite')) {
 	message.reply('Thank you for liking the bot so much that you want to invite it to your server!\nInvite Link: https://tinyurl.com/hr3rucj')
     }
    // if (message.content = ('kys') {
