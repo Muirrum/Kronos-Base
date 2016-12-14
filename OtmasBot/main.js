e2583072109fb134dafa9992459f9024d31fcc40
Otmas <Otmas@users.noreply.github.com>
2016-12-14 16:36:40 -0500

    started a test

--- OtmasBot/main.js
+++ OtmasBot/main.js
@@ -16,7 +16,7 @@ client.on('message', message => {
 
   if (message.author.bot) return;
   //if (!message.content.startsWith(prefix)) return;
-  if (command === 'ping') {
+  if (message.content.startsWith(prefix + 'ping')) {
     console.log('The bot works!')
     message.channel.sendMessage('pong!');
   }
