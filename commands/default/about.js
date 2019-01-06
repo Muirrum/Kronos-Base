803ba2f0272b2fd2af13da6a0fcd28bec23b96d7
Dev-Osmium <oatmas64134@gmail.com>
2019-01-05 20:56:47 -0500

    Add about command

---
+++ commands/default/about.js
@@ -0,0 +1,24 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+
+module.exports = class AboutCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "about",
+            aliases: [],
+            group: "default",
+            memberName: "about",
+            description: "Provides information about the bot",
+            guildOnly: false,
+            examples: ["\\about"]
+        });
+    }
+    run(msg) {
+        const embed = new RichEmbed()
+            .setThumbnail("https://cdn.discordapp.com/attachments/362274609565007872/531289159080607754/kronos.jpg")
+            .addField("Invite:", "https://bit.ly/2SGonmw")
+            .addField("Author", "Dr. Everett Mann#2318")
+            .setTimestamp();
+        return(msg.embed(embed));
+    }
+}
