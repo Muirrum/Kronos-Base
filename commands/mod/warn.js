dc74c0d5b60d5a525e2b8909249d9d99145919ed
Owen Salter <owen@devosmium.xyz>
2019-07-09 22:04:42 -0400

    Add insertion code to warn.js

--- commands/mod/warn.js
+++ commands/mod/warn.js
@@ -1,5 +1,7 @@
 const { Command } = require('discord.js-commando');
 const { RichEmbed } = require('discord.js');
+const SQLite = require("better-sqlite3");
+const warns = new SQLite('./db/warns.sqlite');
 
 module.exports = class WarnCommand extends Command {
     constructor(client) {
@@ -27,7 +29,8 @@ module.exports = class WarnCommand extends Command {
     };
 
     run (msg, { user, reason }) {
-
+        warns.prepare("INSERT INTO warns (user, guild, mod, reason) VALUES (?, ?, ?, ?);").run(user.id, msg.guild.id, msg.author.id, reason);
+        msg.reply(`Warned ${user} for ${reason}`);
     }
 };
 
